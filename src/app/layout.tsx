bdad43b6cbdbea35736fdc8047ec5ac41af51259
Michael Bui <cryst@crystallized.sh>
2025-10-03 23:21:01 +1300

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <head>
+      <link rel="icon" href="/icon.png"/>
+    </head>
       <body
         className={`${pixelOperator.variable} ${windowsBold.variable} ${emojiFont.variable} antialiased`}
       >
