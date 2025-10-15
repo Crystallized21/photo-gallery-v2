df077b6c053d80dd1fceb03cad159f4aa410793e
Michael Bui <cryst@crystallized.sh>
2025-10-15 23:25:29 +1300

    Add full and thumbnail srcs to the API body

--- src/app/api/images/route.ts
+++ src/app/api/images/route.ts
@@ -4,19 +4,36 @@ import { NextResponse } from "next/server";
 import { client } from "@/lib/appwrite";
 
 const storage = new Storage(client);
+const bucketId = process.env.NEXT_PUBLIC_APPWRITE_BUCKET_ID as string;
+const projectId = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID as string;
+const endpoint = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as string;
 
 export async function GET() {
   try {
+    // list all files in the bucket
     const result = await storage.listFiles({
-      bucketId: process.env.NEXT_PUBLIC_APPWRITE_BUCKET_ID as string,
+      bucketId: bucketId,
     });
 
-    const files = result.files.map((file) => ({
-      id: file.$id,
-      name: file.name,
-      src: `${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}/storage/buckets/${process.env.NEXT_PUBLIC_APPWRITE_BUCKET_ID}/files/${file.$id}/view?project=${process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID}`,
-      alt: file.name,
-    }));
+    const files = result.files.map((file) => {
+      // generate thumbnail url
+      const thumbnailUrl = storage.getFilePreview({
+        bucketId: bucketId,
+        fileId: file.$id,
+        quality: 30,
+      });
+
+      // get full resolution url
+      const fullResUrl = `${endpoint}/storage/buckets/${bucketId}/files/${file.$id}/view?project=${projectId}`;
+
+      // return the file json body data
+      return {
+        id: file.$id,
+        alt: file.name,
+        thumbnailSrc: thumbnailUrl,
+        fullResSrc: fullResUrl
+      }
+    });
 
     return NextResponse.json(files);
   } catch (error) {
