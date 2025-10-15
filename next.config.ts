ca234ab940dfbec7e595e26801c09fd10cac2347
Michael Bui <cryst@crystallized.sh>
2025-10-16 00:47:05 +1300

    rename sentry org name

--- next.config.ts
+++ next.config.ts
@@ -21,7 +21,7 @@ export default withGTConfig(
     // For all available options, see:
     // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-    org: "crystallized-project",
+    org: "crystallized",
 
     project: "photo-gallery-rewrite",
 
