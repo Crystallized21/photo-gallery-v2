227d76eca39be66f858e9cd6a5936dbe9eff0833
Michael Bui <cryst@crystallized.sh>
2025-11-29 00:53:44 +1300

    Use suspense on `ImageGallery` component for partial PPR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
+import { Suspense } from "react";
 import FooterInfo from "@/components/FooterInfo";
 import HeaderText from "@/components/HeaderText";
 import ImageGallery from "@/components/image/ImageGallery";
+import LoadingSpinner from "@/components/ui/LoadingSpinner";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-black via-95% to-blue-950">
       <div className="container mx-auto px-4">
         <HeaderText />
-        <ImageGallery />
+        <Suspense fallback={<LoadingSpinner/>}>
+          <ImageGallery />
+        </Suspense>
       </div>
       <FooterInfo />
     </div>
